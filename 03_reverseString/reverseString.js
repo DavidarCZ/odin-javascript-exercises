da1f406a2b6d7854c0e1651c43f1a64c4416bee3
DavidarCZ <94920054+DavidarCZ@users.noreply.github.com>
2025-03-10 14:52:52 +0100

    Finish the reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(quote) {
+    let result = '';
+    for (let i = quote.length - 1; i >= 0; i--) {
+        result += quote[i];
+    }
+    return result;
 };
 
 // Do not edit below this line
