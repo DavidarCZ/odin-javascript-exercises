e2d95588d591e3e411a2474cfb549e29f7b1d83e
DavidarCZ <94920054+DavidarCZ@users.noreply.github.com>
2025-03-19 20:34:40 +0100

    Finished findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,31 @@
-const findTheOldest = function() {
+const findTheOldest = function(persons) {
+    let oldest = persons[0];
+    for (const person of persons) {
+        let ageOldest;
+        let agePerson;
 
+        if(oldest.yearOfDeath === undefined)
+        {
+            ageOldest = new Date().getFullYear() - oldest.yearOfBirth;
+        }else
+        {
+            ageOldest = oldest.yearOfDeath - oldest.yearOfBirth;
+        }
+
+        
+        if(oldest.yearOfDeath === undefined)
+        {
+            agePerson = new Date().getFullYear() - person.yearOfBirth;
+        }else
+        {
+            agePerson = person.yearOfDeath - person.yearOfBirth;
+        }
+
+        if (agePerson > ageOldest) {
+            oldest = person;
+        }
+    }
+    return oldest;
 };
 
 // Do not edit below this line
