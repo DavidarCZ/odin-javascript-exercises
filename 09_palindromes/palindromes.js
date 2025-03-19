0b109ef863a7158118d9e178e659002ab6d442a5
DavidarCZ <94920054+DavidarCZ@users.noreply.github.com>
2025-03-19 19:50:52 +0100

    Finished the palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+    input = input.replace(/[,!.?\s]/g, "").toLowerCase();
+    for (let i = 0; i < input.length/2; i++) {
+        if(input.at(i)!==input.at(input.length-1-i)){
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
