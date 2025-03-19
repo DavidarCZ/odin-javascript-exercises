bb5f211aba92430d2ff1b94a1c9d1bcd66e4e4e2
DavidarCZ <94920054+DavidarCZ@users.noreply.github.com>
2025-03-19 14:49:15 +0100

    Finish the calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(x,y) {
+	return x+y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x,y) {
+	return x-y;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  let sum = 0;
+  numbers.forEach((element) => {
+    if (!isNaN(Number(element))) {
+      sum += Number(element);
+    }
+  });
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  return numbers.reduce((result,element) => {
+    return result * element;
+  },1);
 };
 
-const power = function() {
-	
+const power = function(x,y) {
+  let result = x
+	for (let i = 1; i < y; i++) {
+    result *= x;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	let fact = 1;
+  for (let i = 1; i <= x; i++) {
+    fact *=i;
+  }
+  return fact
 };
 
 // Do not edit below this line
